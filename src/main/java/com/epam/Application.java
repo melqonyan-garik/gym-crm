3e94fbbe441687f5ef4b954ee3736d7497f1f611
Garik Melqonyan <garik_melqonyan@epam.com>
2023-11-21 13:57:34 +0400

    remove context variable

--- src/main/java/com/epam/Application.java
+++ src/main/java/com/epam/Application.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class Application {
     public static void main(String[] args) {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+        new AnnotationConfigApplicationContext(AppConfig.class);
 
     }
 }
